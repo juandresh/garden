2da34ba137c483fa60f50718d7ddbdceecdb5e2e
juandresh <jujimenezve@unal.edu.co>
2026-01-30 00:01:46 -0500

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,12 @@ import TulipanMorado from './components/tulipan/tulipan_morado'
 import TulipanRosado from './components/tulipan/tulipan_rosado'
 import Cerca from './assets/items/cerca.svg'
 import Nube from './assets/items/nube.svg'
+import Reproductor from './components/reproductor/reproductor'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mostrarModal, setMostrarModal] = useState(false)
 
  return (
     <div className="layout">
@@ -24,40 +25,67 @@ function App() {
       
       <main className="columna centro">
 
-        <h1 className="titulo">Mi Jardín</h1>
+        <h1 className="titulo">❤️ Un Jardín para mi Novia ❤️</h1>
 
-        <div className='reproductor'>
 
-        </div>
+        <Reproductor className='reproductor'></Reproductor>
+
 
         <div className='cielo'>
-          <div className='sol'></div>
+          <div className="sol" 
+          onClick={() => setMostrarModal(true)}
+          />
+
           <img src={Nube} className="nube1" />
           <img src={Nube} className="nube2" />
         </div>
 
         <img src={Cerca} className="cerca" />
 
-        <div className="jardin">
-          <Girasol className="gir1"></Girasol>
-          <TulipanRosado className="tup1"></TulipanRosado>
-          <Lirio className="lir1"></Lirio>
-          <RosaRosado className="ros1"></RosaRosado>
-          <TulipanAmarillo className="tup2"></TulipanAmarillo>
-          <Orquidea className="orq1"></Orquidea>
-          <Lirio className="lir2"></Lirio>
-          <TulipanBlanco className="tup3"></TulipanBlanco>
-          <RosaAmarillo className="ros2"></RosaAmarillo>
-          <RosaBlanco className="ros3"></RosaBlanco>
-          <TulipanMorado className="tup4"></TulipanMorado>
-          <Orquidea className="orq2"></Orquidea>
-          <Lirio className="lir3"></Lirio>
-          <RosaRosado className="ros4"></RosaRosado>
-          <Girasol className="gir2"></Girasol>
-          <TulipanBlanco className="tup5"></TulipanBlanco>
-          <TulipanMorado className="tup6"></TulipanMorado>
+        <Girasol className="gir1"></Girasol>
+        <TulipanRosado className="tup1"></TulipanRosado>
+        <Lirio className="lir1"></Lirio>
+        <RosaRosado className="ros1"></RosaRosado>
+        <TulipanAmarillo className="tup2"></TulipanAmarillo>
+        <Orquidea className="orq1"></Orquidea>
+        <Lirio className="lir2"></Lirio>
+        <TulipanBlanco className="tup3"></TulipanBlanco>
+        <RosaAmarillo className="ros2"></RosaAmarillo>
+        <RosaBlanco className="ros3"></RosaBlanco>
+        <TulipanMorado className="tup4"></TulipanMorado>
+        <Orquidea className="orq2"></Orquidea>
+        <Lirio className="lir3"></Lirio>
+        <RosaRosado className="ros4"></RosaRosado>
+        <Girasol className="gir2"></Girasol>
+        <TulipanBlanco className="tup5"></TulipanBlanco>
+        <TulipanMorado className="tup6"></TulipanMorado>
+        <TulipanRosado className="tup7"></TulipanRosado>
+        <Girasol className="gir3"></Girasol>
 
+        <div className="jardin">
         </div>
+
+        {mostrarModal && (
+          <div className="modal-overlay" onClick={() => setMostrarModal(false)}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
+              <p>Solo quiero recordarte lo importante que eres para mí y lo mucho que te amo. 
+                Este tiempo a tu lado ha sido para mí de lo más maravilloso, y tengo toda la 
+                seguridad de que seguirá siendo así. Desde que te conocí, todo ha mejorado para 
+                mí, he encontrado un sentido para hacer las cosas con amor, algo que me hace 
+                sentir feliz y realizado. Sin duda, conocer a una mujer tan inteligente, 
+                hermosa, especial y maravillosa ha sido lo mejor que me ha podido pasar. 
+                Me llena de alegría el pensar en nuestro futuro juntos, yo realizándome 
+                contigo a mi lado y tú realizándote conmigo a tu lado. Te amo mucho, 
+                mi noviecita hermosa ❤️</p>
+
+              <p></p>
+
+              <button onClick={() => setMostrarModal(false)}>
+                Cerrar
+              </button>
+            </div>
+          </div>
+        )}
 
       </main>
 
---
+++ src/components/reproductor/reproductor.jsx
@@ -0,0 +1,68 @@
+import { useRef, useState, useEffect } from "react"
+import "./reproductor.css"
+import Play from "/src/assets/items/botonSonando.svg"
+import Pause from "/src/assets/items/botonPausa.svg"
+import Next from "/src/assets/items/botonSig.svg"
+import Prev from "/src/assets/items/botonAnt.svg"
+
+const canciones = [
+    "/src/assets/songs/1.mp3",  // your'e my best friend
+    "/src/assets/songs/2.mp3",  // stupid love story
+    "/src/assets/songs/3.mp3",  // runaway
+    "/src/assets/songs/4.mp3",  // your eyes tell
+    "/src/assets/songs/5.mp3",  // puntos suspensivos
+    "/src/assets/songs/6.mp3",  // te invito
+    "/src/assets/songs/7.mp3",  // lago en el cielo
+    "/src/assets/songs/8.mp3"   // loco por tu amor
+]
+
+export default function Reproductor() {
+    const audioRef = useRef(null)
+    const [indice, setIndice] = useState(0)
+    const [reproduciendo, setReproduciendo] = useState(false)
+
+    const playPause = () => {
+    if (reproduciendo) {
+        audioRef.current.pause()
+    } else {
+        audioRef.current.play()
+    }
+    setReproduciendo(!reproduciendo)
+    }
+
+    const siguiente = () => {
+    setIndice((i) => (i + 1) % canciones.length)
+    }
+
+    const anterior = () => {
+    setIndice((i) => (i === 0 ? canciones.length - 1 : i - 1))
+    }
+
+    useEffect(() => {
+    if (reproduciendo) {
+        audioRef.current.play()
+    }
+    }, [indice])
+
+    return (
+        <div className="rep">
+            <audio ref={audioRef} src={canciones[indice]} />
+
+            <button onClick={anterior}>
+                <img src={Prev} alt="Anterior" className="botons"/>
+            </button>
+
+            <button onClick={playPause}>
+                <img
+                src={reproduciendo ? Pause : Play}
+                alt="Play / Pause" 
+                className="botonp"
+                />
+            </button>
+
+            <button onClick={siguiente}>
+                <img src={Next} alt="Siguiente" className="botons"/>
+            </button>
+        </div>
+    )
+}
